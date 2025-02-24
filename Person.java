5a5e770be3d814cc33d107d43c584a80d9f6dc30
Ian Teigen <itiegen26@students.polytechnic.org>
2025-02-24 09:11:49 -0800

    Initial Person and Tester

---
+++ Person.java
@@ -0,0 +1,34 @@
+package RideShare;
+public class Person{
+    //fields
+    private int destination;
+    private boolean direction; //true for going right, false for left
+    private int startLocation;
+
+    //constructors
+    public Person(int myDestination, int myStartLocation){
+        destination = myDestination;
+        startLocation=myStartLocation;
+        direction = (destination>startLocation);
+        
+    }
+
+    //methods
+
+    public int getDestination(){
+        return destination;
+    }
+    public boolean getDirection(){
+        return direction;
+    }
+    public int startLocation(){
+        return startLocation;
+    }
+
+    public String toString(){
+        return super.toString() + "Start: " +startLocation +" Destination: " + destination + " Going right? " +direction;
+    }
+
+
+
+}
